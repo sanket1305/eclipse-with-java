1e72e1b3d60b4da262469ed65c76bd601736a8ec
sanket1305 <meetsanket24@gmail.com>
2021-11-08 12:47:57 +0530

    Updated the toString Method

--- src/org/sank/model/User.java
+++ src/org/sank/model/User.java
@@ -20,6 +20,11 @@ public class User {
 	public void setAge(int age) {
 		this.age = age;
 	}
+
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", age=" + age + "]";
+	}
 	
 	
 }
